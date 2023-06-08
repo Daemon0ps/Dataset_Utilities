3cfe6fba18f563d9e42d7046b2967860ffea3215
Daemon0ps <133270668+Daemon0ps@users.noreply.github.com>
2023-06-07 19:57:42 -0700

    Create colormod.py
    COLORS!
    
    use cp_y("text") for yellow, etc.,
    
    instead of print("lame gray")

---
+++ colormod.py
@@ -0,0 +1,234 @@
+from termcolor import cprint
+
+
+@staticmethod
+def cp_lf(): 
+    try:
+        cs =  cprint("", "black") 
+        return cs
+    except Exception as e: pass
+    finally: pass
+@staticmethod
+def cp_bk(x): 
+    try:
+        cs =  cprint(x, "black",end="") 
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_r(x): 
+    try:
+        cs =  cprint(x, "red",end="") 
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_g(x): 
+    try:
+        cs =  cprint(x, "green",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_y(x): 
+    try:
+        cs =  cprint(x, "yellow",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_b(x): 
+    try:
+        cs =  cprint(x, "blue",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_m(x): 
+    try:
+        cs =  cprint(x, "magenta",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_c(x): 
+    try:
+        cs =  cprint(x, "cyan",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_w(x): 
+    try:
+        cs =  cprint(x, "white",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_lgr(x): 
+    try:
+        cs =  cprint(x, "light_grey",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_dg(x): 
+    try:
+        cs =  cprint(x, "dark_grey",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_lr(x): 
+    try:
+        cs =  cprint(x, "light_red",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_lg(x): 
+    try:
+        cs =  cprint(x, "light_green",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_ly(x): 
+    try:
+        cs =  cprint(x, "light_yellow",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_lb(x): 
+    try:
+        cs =  cprint(x, "light_blue",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_lm(x): 
+    try:
+        cs =  cprint(x, "light_magenta",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_lc(x): 
+    try:
+        cs =  cprint(x, "light_cyan",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_obk(x): 
+    try:
+        cs =  cprint(x, "on_black",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_or(x): 
+    try:
+        cs =  cprint(x, "on_red",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_og(x): 
+    try:
+        cs =  cprint(x, "on_green",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_oy(x): 
+    try:
+        cs =  cprint(x, "on_yellow",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod
+def cp_ob(x): 
+    try:
+        cs =  cprint(x, "on_blue",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_om(x): 
+    try:
+        cs =  cprint(x, "on_magenta",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_oc(x): 
+    try:
+        cs =  cprint(x, "on_cyan",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_ow(x): 
+    try:
+        cs =  cprint(x, "on_white",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_olgr(x): 
+    try:
+        cs =  cprint(x, "on_light_grey",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_odg(x): 
+    try:
+        cs =  cprint(x, "on_dark_grey",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_olr(x): 
+    try:
+        cs =  cprint(x, "on_light_red",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_olg(x): 
+    try:
+        cs =  cprint(x, "on_light_green",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_oly(x): 
+    try:
+        cs =  cprint(x, "on_light_yellow",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_olb(x): 
+    try:
+        cs =  cprint(x, "on_light_blue",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_olm(x):
+    try:
+        cs =  cprint(x, "on_light_magenta",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
+@staticmethod    
+def cp_olc(x): 
+    try:
+        cs =  cprint(x, "on_light_cyan",end="")
+        return cs
+    except Exception as e:pass
+    finally:pass
